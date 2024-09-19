5ed8e253dae70c161c561c57af87c7743f9f548e
JohnCreateITLLC <100145838+JohnCreateITLLC@users.noreply.github.com>
2024-09-18 23:56:20 -0600

    Update crimeometer-google-maps-control-v3.js

--- crimeometer-google-maps-control-v3.js
+++ crimeometer-google-maps-control-v3.js
@@ -2,10 +2,10 @@
 *    crimeometer-google-maps-control-v3.js
 *    Data Visualization with Google
 */
-  
+
 function initMap() {
 
-    //Load parameters
+    // Load parameters
     var script_tag = document.getElementById('crimeometer-google-maps-control-v3');
     var riskmap_index = script_tag.getAttribute("riskmap-index");
     var data_tk = script_tag.getAttribute("data-tk");
@@ -17,19 +17,26 @@ function initMap() {
     var data_datetime_end = script_tag.getAttribute("data-datetime-end");
     var data_max_incidents = script_tag.getAttribute("data-max-incidents");
 
-    //Map settings
+    // Map settings
     var map;
 
-    const origin_lat = data_lat; 
-    const origin_lng = data_lon;
+    const origin_lat = parseFloat(data_lat); 
+    const origin_lng = parseFloat(data_lon);
     const zoom = parseInt(data_zoom);
 
     map = new google.maps.Map(
-        document.getElementById('crime-map'),{
+        document.getElementById('crime-map'), {
             center: new google.maps.LatLng(origin_lat, origin_lng), 
             zoom: zoom, 
             fullscreenControl: false
         });
+
+    // Add a marker at the initial center
+    var marker = new google.maps.Marker({
+        position: { lat: origin_lat, lng: origin_lng },
+        map: map,
+        title: 'Initial Location'
+    });
 
     var logoControlDiv = document.createElement('div');
     var logoControl = new MyLogoControl(logoControlDiv);
@@ -41,7 +48,7 @@ function initMap() {
     graphicControlDiv.index = 1;
     map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(graphicControlDiv);
 
-    if (data_tk != ''){
+    if (data_tk != '') {
 
         var layer;
 
@@ -50,25 +57,23 @@ function initMap() {
         layer = new google.maps.ImageMapType({
             name: 'layerID',
             getTileUrl: function(coord, zoom) {
-              //console.log(coord);
-              var url_tile = TILE_URL
-                .replace('{index}',riskmap_index)
-                .replace('{x}', coord.x)
-                .replace('{y}', coord.y)
-                .replace('{z}', zoom)
-                .replace('{tk}',data_tk);
-              return url_tile;
+                var url_tile = TILE_URL
+                    .replace('{index}', riskmap_index)
+                    .replace('{x}', coord.x)
+                    .replace('{y}', coord.y)
+                    .replace('{z}', zoom)
+                    .replace('{tk}', data_tk);
+                return url_tile;
             },
             tileSize: new google.maps.Size(256, 256),
             minZoom: 1,
             maxZoom: 20
         });
 
-       map.overlayMapTypes.push(layer); 
-
+        map.overlayMapTypes.push(layer); 
     }
 
-    if (x_api_key != '' && x_api_key != '{YOUR API KEY}'){
+    if (x_api_key != '' && x_api_key != '{YOUR API KEY}') {
 
         var request = new XMLHttpRequest();
 
@@ -86,26 +91,25 @@ function initMap() {
         // crimeometer-handler-v2 URL
         const url_level1 = 'https://api.crimeometer.com/v2/handler/level1';
 
-        request.onreadystatechange=function(){
-            if (request.readyState==4 && request.status==200){
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
                 
                 let res = JSON.parse(request.responseText);
                 result_data = res['incidents'];
             
-                //Load data.
+                // Load data.
                 var features = [];
                 var info = [];
                 for (var i = 0; i < result_data.length; i++) {
                     var item = {
                         position: new google.maps.LatLng(result_data[i].lat, result_data[i].lon),
                         type: result_data[i].offense,
-                        }
+                    }
                     features.push(item)
                     var item_info = {
                         content: result_data[i].offense
                     }
                     info.push(item_info)
-            
                 }
 
                 var infowindow = new google.maps.InfoWindow();
@@ -125,17 +129,15 @@ function initMap() {
                             infowindow.open(map, marker);
                         }
                     })(marker, i));
-            
                 };
             }
-
         }
+
         request.open("POST", url_level1);
-        request.setRequestHeader('Content-Type','aplication/json')
-        request.setRequestHeader('x-api-key',x_api_key)
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.setRequestHeader('x-api-key', x_api_key);
         request.send(JSON.stringify(payload));
     }
-    
 }
 
 function MyLogoControl(controlDiv) {
@@ -150,14 +152,12 @@ function MyLogoControl(controlDiv) {
     if (window.location.hostname == '' || window.location.hostname == null){
         source = window.location.href;
     }
-    var url = 'https://crimeometer.com/?utm_medium=embeddablemap&utm_source='+source;
+    var url = 'https://crimeometer.com/?utm_medium=embeddablemap&utm_source=' + source;
 
     google.maps.event.addDomListener(logo, 'click', function() {
         window.open(url, '_blank');
     });
 }
-
-
 
 function MyGraphicControl(controlDiv) {
     controlDiv.style.padding = '10px';
